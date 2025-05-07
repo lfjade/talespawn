0d293d844715595a6d5681858dab216199b00553
physi1csb1tch <fernandes.l.jade@gmail.com>
2025-05-07 01:38:07 -0300

    html not loading

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const {app, BrowserWindow, icpMain} = require('electron')
+const path = require('path')
+
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    mainWindow.loadFile('Telas/index.html')
+}
+
+app.whenReady().then(createMainWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin'){
+        app.quit()
+    }
+})
